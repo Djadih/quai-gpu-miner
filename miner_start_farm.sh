252ad8c5aa706e1161008d51a03f2b67b688b5ee
kiltsonfire <106615399+kiltsonfire@users.noreply.github.com>
2023-10-29 12:41:24 -0500

    Update miner_start_farm.sh
    spread out hash to all slices

--- miner_start_farm.sh
+++ miner_start_farm.sh
@@ -33,35 +33,35 @@ if [[ "$1" == "00" ]]; then
 
 elif [[ "$1" == "01" ]]; then
    #  run_miner 0 1 35.184.84.143 30001
-    run_miner 0 1 34.168.1.153 30000
+    run_miner 0 1 34.168.1.153 30001
 
 elif [[ "$1" == "02" ]]; then
    #  run_miner 0 2 35.184.84.143 30002
-    run_miner 0 2 34.168.1.153 30000
+    run_miner 0 2 34.168.1.153 30002
 
 elif [[ "$1" == "10" ]]; then
    #  run_miner 1 0 35.184.84.143 30003
-    run_miner 1 0 34.168.1.153 30000
+    run_miner 1 0 34.168.1.153 30003
 
 elif [[ "$1" == "11" ]]; then
    #  run_miner 1 1 35.184.84.143 30004
-    run_miner 1 1 34.168.1.153 30000
+    run_miner 1 1 34.168.1.153 30004
 
 elif [[ "$1" == "12" ]]; then
    #  run_miner 1 2 35.184.84.143 30005
-    run_miner 1 2 34.168.1.153 30000
+    run_miner 1 2 34.168.1.153 30005
 
 elif [[ "$1" == "20" ]]; then
    #  run_miner 2 0 35.184.84.143 30006
-    run_miner 2 0 34.168.1.153 30000
+    run_miner 2 0 34.168.1.153 30006
 
 elif [[ "$1" == "21" ]]; then
    #  run_miner 2 1 35.184.84.143 30007
-    run_miner 2 1 34.168.1.153 30000
+    run_miner 2 1 34.168.1.153 30007
 
 elif [[ "$1" == "22" ]]; then
    #  run_miner 1 2 35.184.84.143 30008
-    run_miner 2 2 34.168.1.153 30000
+    run_miner 2 2 34.168.1.153 30008
 
 else
     echo "Invalid miner_num value"
